095a1bf0a3de88d2734249bb0f70277516da7bb2
Ricardo Pinheiro <rpinheiro.033@gmail.com>
2024-11-14 19:56:04 +0000

    Creation of a type to minimize errors on retreival of data

--- weather-app/src/main.ts
+++ weather-app/src/main.ts
@@ -3,22 +3,60 @@ import typescriptLogo from './typescript.svg'
 import viteLogo from '/vite.svg'
 import { setupCounter } from './counter.ts'
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
-  </div>
-`
+const WEATHER_API_KEY:string = 'BASJADVSDHVKYR4ZG8KET3SEJ';
+const LINK_TO_FETCH_DATA:string = 'https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/london?key='
 
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+// document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
+//   <div>
+//     <a href="https://vite.dev" target="_blank">
+//       <img src="${viteLogo}" class="logo" alt="Vite logo" />
+//     </a>
+//     <a href="https://www.typescriptlang.org/" target="_blank">
+//       <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
+//     </a>
+//     <h1>Vite + TypeScript</h1>
+//     <div class="card">
+//       <button id="counter" type="button"></button>
+//     </div>
+//     <p class="read-the-docs">
+//       Click on the Vite and TypeScript logos to learn more
+//     </p>
+//   </div>
+// `
+
+// setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+
+type WeatherData = {
+  description: string,
+  currentConditions: {
+    temp: number
+  }
+}
+
+const fetchWeatherData = async():Promise<WeatherData> => {
+  const weatherResultsDiv:HTMLElement = document.getElementById('weather-results')!;
+  let weatherData;
+  try {
+    const fetchWeatherData = await fetch(LINK_TO_FETCH_DATA + WEATHER_API_KEY, 
+      {
+        method: 'GET',
+        mode: 'cors'
+      }
+    )
+
+    if(!fetchWeatherData.ok)
+      throw new Error('Something went wrong.');
+
+    weatherData = await fetchWeatherData.json();
+    // console.log(consumableWeatherData.description);
+    // console.log(consumableWeatherData.currentConditions.temp);
+    
+  } catch(error) {
+    console.error('Something went wrong');
+  }
+  
+  return weatherData;
+};
+
+const result = await fetchWeatherData();
+console.log(result.description);
